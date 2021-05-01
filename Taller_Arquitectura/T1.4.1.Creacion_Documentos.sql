ea5134f71c7a862db6666e87ae5c394728e5f9fb
Natalia López Gallego <nalopezga@unal.edu.co>
2021-04-30 21:57:34 -0500

    SQL

---
+++ Taller_Arquitectura/T1.4.1.Creacion_Documentos.sql
@@ -0,0 +1,22 @@
+db.Customer.insertMany(
+    {ID:"1216725342", IdSex:"F", Age:"76", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"13131313", IdSex:"F", Age:"80", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"32317704", IdSex:"F", Age:"45", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"134279287", IdSex:"M", Age:"25", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"1152219515", IdSex:"F", Age:"20", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"1152219514", IdSex:"F", Age:"22", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"1152219512", IdSex:"M", Age:"24", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"1152219518", IdSex:"M", Age:"29", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"10293774", IdSex:"M", Age:"80", Driving_License:"1", IdRegion:"34.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"354788", IdSex:"M", Age:"50", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"766565", IdSex:"F", Age:"30", Driving_License:"1", IdRegion:"28.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"999088", IdSex:"F", Age:"20", Driving_License:"1", IdRegion:"17.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"36770.0", IdChannel:"13.0",Vintage:"15", Response:"0"},
+    {ID:"1040752831", IdSex:"M", Age:"56", Driving_License:"1", IdRegion:"32.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"34570.0", IdChannel:"15.0",Vintage:"15", Response:"1"},
+    {ID:"1036653398", IdSex:"F", Age:"26", Driving_License:"1", IdRegion:"32.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"94770.0", IdChannel:"15.0",Vintage:"15", Response:"0"},
+    {ID:"98631127", IdSex:"M", Age:"43", Driving_License:"1", IdRegion:"12.0", Previously_Insured:"1", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"4570.0", IdChannel:"25.0",Vintage:"10", Response:"0"},
+    {ID:"43838109", IdSex:"F", Age:"56", Driving_License:"1", IdRegion:"22.0", Previously_Insured:"1", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"124590.0", IdChannel:"19.0",Vintage:"15", Response:"1"},
+    {ID:"32334053", IdSex:"F", Age:"76", Driving_License:"1", IdRegion:"32.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"No", Annual_Premium:"994570.0", IdChannel:"15.0",Vintage:"15", Response:"1"},
+    {ID:"42964202", IdSex:"M", Age:"52", Driving_License:"1", IdRegion:"12.0", Previously_Insured:"1", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"4570.0", IdChannel:"25.0",Vintage:"10", Response:"0"},
+    {ID:"43507251", IdSex:"F", Age:"26", Driving_License:"1", IdRegion:"22.0", Previously_Insured:"1", Vehicle_Age:"1-2 Year", Vehicle_Damage:"Yes", Annual_Premium:"124590.0", IdChannel:"19.0",Vintage:"15", Response:"1"},
+    {ID:"1036777111", IdSex:"F", Age:"36", Driving_License:"1", IdRegion:"32.0", Previously_Insured:"0", Vehicle_Age:"1-2 Year", Vehicle_Damage:"No", Annual_Premium:"994570.0", IdChannel:"15.0",Vintage:"15", Response:"1"}
+);
